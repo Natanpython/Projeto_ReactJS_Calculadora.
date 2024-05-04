9c4865adfdf82c6e27a07e6baeda7a7450547057
Natanael <natanaelpereira745@gmail.com>
2024-05-04 19:18:17 -0300

    Treinando conceitos ReactJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import Input from './components/Input';
+import Button from './components/Button';
 
-function App() {
+import { Container, Content, Row } from "./style";
+import { useState } from 'react';
+ 
+const App = () => {
+  const [CurrentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setfirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+
+
+  const handleOnClear = ( ) =>{
+    setCurrentNumber('0')
+    setfirstNumber('0')
+    
+  }
+  const handleAddNumber = (number) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev }${number}`)
+  }
+
+  const headleSumNumbers = () => {
+
+    if(firstNumber === '0'){
+      setfirstNumber(String(CurrentNumber));
+      setCurrentNumber('0')
+      setOperation('+')
+    }else {
+      const sum = Number(firstNumber) + Number(CurrentNumber)
+      setCurrentNumber(String(sum))
+      
+    }
+  }
+  const headleMinusNumbers = () => {
+
+    if(firstNumber === '0'){
+      setfirstNumber(String(CurrentNumber));
+      setCurrentNumber('0')
+      setOperation('-')
+    }else {
+      const sum = Number(firstNumber) - Number(CurrentNumber)
+      setCurrentNumber(String(sum))
+      
+    }
+  }
+  const headleMultNumbers = () => {
+
+    if(firstNumber === '0'){
+      setfirstNumber(String(CurrentNumber));
+      setCurrentNumber('0')
+      setOperation('*')
+    }else {
+      const sum = Number(firstNumber) * Number(CurrentNumber)
+      setCurrentNumber(String(sum))
+      
+    }
+  }
+  const headleDivNumbers = () => {
+
+    if(firstNumber === '0'){
+      setfirstNumber(String(CurrentNumber));
+      setCurrentNumber('0')
+      setOperation('/')
+    }else {
+      const sum = Number(firstNumber) / Number(CurrentNumber)
+      setCurrentNumber(String(sum))
+      
+    }
+  }
+
+  const headleEquale = () => {
+
+    if(firstNumber !== '0' && operation !== '' && CurrentNumber !== '0'){
+      switch(operation){
+        case '+' :
+          headleSumNumbers();
+          break;
+        case '-' :
+          headleMinusNumbers();
+          break;
+        case '*' :
+          headleMultNumbers();
+            break;
+        case '/' :
+          headleDivNumbers();
+          break;
+        default:
+          break;
+
+      }
+      
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container> 
+        <Content>
+          <Input value={CurrentNumber}/>
+          <Row>
+            <Button label="x" onClick={headleMultNumbers}/>
+            <Button label="/" onClick={headleDivNumbers}/>
+            <Button label="." onClick={() => handleAddNumber('.')}/>
+            <Button label="DEL" onClick={handleOnClear }/>
+          </Row>
+          <Row>
+            <Button label="7" onClick={() => handleAddNumber('7')}/>
+            <Button label="8" onClick={() => handleAddNumber('8')}/>
+            <Button label="9" onClick={() => handleAddNumber('9')}/>
+            <Button label="+" onClick={headleSumNumbers}/>
+          </Row>
+          <Row>
+            <Button label="4" onClick={() => handleAddNumber('4')}/>
+            <Button label="5" onClick={() => handleAddNumber('5')}/>
+            <Button label="6" onClick={() => handleAddNumber('6')}/>
+            <Button label="-" onClick={headleMinusNumbers}/>
+          </Row>
+         
+          <Row>
+            <Button label="1" onClick={() => handleAddNumber('1')}/>
+            <Button label="2" onClick={() => handleAddNumber('2')}/>
+            <Button label="3" onClick={() => handleAddNumber('3')}/>
+            <Button label="=" onClick={headleEquale}/>
+          </Row>
+          <Row>
+            <Button label="0" onClick={() => handleAddNumber('0')}/>
+            
+          </Row>
+         
+        </Content>
+      
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Input/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #fff;
+   
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #fff;
+        border: 0; 
+        font-size: 24px;
+        font-family: 'Roboto';
+        padding: 0 10px;
+    }
+
+
+`
